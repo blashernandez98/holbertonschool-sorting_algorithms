381425dc568b4e1af4510973addbccf9e0f5b208
Blas <blashernandez98@gmail.com>
2022-08-23 09:16:14 -0700

    Task 0 fix null array case

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,17 +11,20 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	int aux, ordered = 1;
 
-	for (i = 0; i < size - 1; i++)
+	if (array)
 	{
-		if (array[i] > array[i + 1])
+		for (i = 0; i < size - 1; i++)
 		{
-			aux = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = aux;
-			print_array(array, size);
-			ordered = 0;
+			if (array[i] > array[i + 1])
+			{
+				aux = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = aux;
+				print_array(array, size);
+				ordered = 0;
+			}
 		}
+		if (!ordered)
+			bubble_sort(array, size);
 	}
-	if (!ordered)
-		bubble_sort(array, size);
 }
