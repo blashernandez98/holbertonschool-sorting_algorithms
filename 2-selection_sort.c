8dd823194629a9e92eb0a8359eed3f446c8252b1
Blas <blashernandez98@gmail.com>
2022-08-24 10:10:14 -0700

    Task 2 - Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts array of integers with selection sort algo.
+ * @array: Array of integers.
+ * @size: Size of array.
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int min, min_pos = -1, i, j;
+
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			min = array[i];
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[j] < min)
+				{
+					min = array[j];
+					min_pos = j;
+				}
+			}
+			if (min < array[i])
+			{
+				array[min_pos] = array[i];
+				array[i] = min;
+				print_array(array, size);
+			}
+		}
+	}
+}
